5d8acede26de9fa73ae6ec4a59e320dec2632007
Carlos Leite - cadu <caduado@gmail.com>
2020-08-10 06:29:47 -0300

    fix pep8 issues

--- merge2pdf.py
+++ merge2pdf.py
@@ -7,17 +7,18 @@ from PyPDF4 import PdfFileReader
 from PyPDF4 import PdfFileWriter
 
 from reportlab.lib.pagesizes import A4
-from reportlab.pdfgen import canvas 
+from reportlab.pdfgen import canvas
 
 
 class MergeToPdf:
 
-    def __init__(self, paths_list:list , output_file_path: str = 'outputfile.pdf'):
+    def __init__(self, paths_list: list, output_file_path: str = 'outputfile.pdf'):
         '''
         A class to merge PDFs files. Pass a lista of File Paths to be merged
 
         Args:
-            paths_list (list): a list of paths(string) ou a lists of tuples like (<path[str]>, <pagerange[tuple(page_ini, page_end)]>
+            paths_list (list): a list of paths(string) ou a lists of tuples
+                      like (<path[str]>, <pagerange[tuple(page_ini, page_end)]>
             output_file_path (str, optional): a string defining the outputpath.
         '''
         self.paths_list = paths_list
@@ -26,10 +27,10 @@ class MergeToPdf:
     def _image_to_page_(self, image_path):
         '''
         "draw" a image inside a PDF Page
-        
-        the reason to djust not use PIL was aesthetic, 
+
+        the reason to djust not use PIL was aesthetic,
         to have an image inside a real PDF page not just an image converted to PDF
-        
+
         todo: an option to rotate the image  if H > w.
         todo: an option to PDF Page Size
 
@@ -41,24 +42,24 @@ class MergeToPdf:
         '''
 
         imgTemp = BytesIO()
-        imgDoc = canvas.Canvas(imgTemp, pagesize=A4) # todo: param page size
+        imgDoc = canvas.Canvas(imgTemp, pagesize=A4)  # todo: param page size
         imgDoc.drawImage(image_path, 20, 420)  # todo: param margin
         imgDoc.save()
         return PdfFileReader(BytesIO(imgTemp.getvalue()))
 
-    def _path_decople_(self, path_list_item)->tuple:
+    def _path_decople_(self, path_list_item) -> tuple:
         '''
         Decomple the file path list into the FILE PATH and Page range ..
-        
-        the list could be 
-        
+
+        the list could be
+
             [
-                ('path_to_file', (pag_start, pag_ends)),  
+                ('path_to_file', (pag_start, pag_ends)),
                 ...
             ]
-        
+
         to indicade the range of pages of a pdf to be merged
-        
+
         Args:
             path_list_item ([type]): a list os files path
 
@@ -74,24 +75,24 @@ class MergeToPdf:
         '''
         merge files into a PDF
 
-        # todo: check file type 
+        # todo: check file type
         '''
-        merged_pdf= PdfFileMerger()
-        
+        merged_pdf = PdfFileMerger()
+
         for file_path in self.paths_list:
             file_name, page_range = self._path_decople_(file_path)
             if file_name.lower().endswith(('.png', '.jpg', '.jpeg')):
-                merged_pdf.append(fileobj = self._image_to_page_(file_name))
+                merged_pdf.append(fileobj=self._image_to_page_(file_name))
             else:
                 if page_range:
-                    merged_pdf.append(fileobj = file_name, pages = page_range)
+                    merged_pdf.append(fileobj=file_name, pages=page_range)
                 else:
-                    merged_pdf.append(fileobj = file_name)
+                    merged_pdf.append(fileobj=file_name)
 
         # write to outputfile
-        output = open(self.output_file_path, 'wb') # output file
-        merged_pdf.write(output) # write merge content to file
-        output.close() 
+        output = open(self.output_file_path, 'wb')  # output file
+        merged_pdf.write(output)  # write merge content to file
+        output.close()
         merged_pdf.close()
 
 
--- tests/test_merge2pdf.py
+++ tests/test_merge2pdf.py
@@ -1,5 +1,4 @@
 import unittest
-import datetime
 
 from PyPDF4 import PdfFileReader
 
@@ -23,12 +22,12 @@ class TestClassMerge2Pdf(unittest.TestCase):
         '''
         '''
         image_paths = [
-            ('filepat.pdf', (0,1)),
+            ('filepat.pdf', (0, 1)),
         ]
         m = MergeToPdf(paths_list=image_paths)
         r = m._path_decople_(image_paths[0])
 
-        self.assertEqual(r, tuple(('filepat.pdf', (0,1))), msg='page_range fail' )
+        self.assertEqual(r, tuple(('filepat.pdf', (0, 1))), msg='page_range fail')
 
     def test_decomple_no_page_range(self):
         '''
@@ -42,20 +41,21 @@ class TestClassMerge2Pdf(unittest.TestCase):
         self.assertEqual(r, tuple(('filepath.pdf', None)))
 
     def test_merge_pdf_output(self):
-        import os
+
         image_paths = [
             'tests/pdf_samples/jpeg_w_350.jpg',
             'tests/pdf_samples/pdf_sample_A Sample PDF_loremIpsum_pages_01.pdf',
             'tests/pdf_samples/pdf_sample_b_pages_01.pdf',
             'tests/pdf_samples/pdf_sample_dummy_w3c_pages_01.pdf',
             'tests/pdf_samples/pdf_sample_googledocs_image_pages_02.pdf',
-            # 'tests/pdf_samples/pdf_sample_googlesheet_pages_02.pdf', # fail to read  invalid literal for int() with base 10: b'F-1.4' !!!
+            ## the next PDF fail to read - invalid literal for int() with base 10: b'F-1.4' !!!
+            # 'tests/pdf_samples/pdf_sample_googlesheet_pages_02.pdf',
             'tests/pdf_samples/pdf_sample_libreoffice_exported_ISO19005_pages_02.pdf',
             'tests/pdf_samples/pdf_sample_libreoffice_exported_format_FDF_pages_02.pdf',
             'tests/pdf_samples/pdf_sample_libreoffice_exported_hibrid_format_pages_02.pdf',
             'tests/pdf_samples/pdf_sample_libreoffice_exported_not_hybrid_ISO19005_pages_02.pdf',
             'tests/pdf_samples/pdf_sample_pages_01.pdf',
-            ('tests/pdf_samples/pdf_sample_readthedocs_pdf_networkdays_pages_019.pdf', (0,2)),
+            ('tests/pdf_samples/pdf_sample_readthedocs_pdf_networkdays_pages_019.pdf', (0, 2)),
             'tests/pdf_samples/pdf_sample_text_edit_macos_pages_01.pdf',
             'tests/pdf_samples/pdf_sample_wikimedia_org_pages_01.pdf',
             'tests/pdf_samples/sample_pdf_commandline_xhtml2pdf_generated_pages_01.pdf',
